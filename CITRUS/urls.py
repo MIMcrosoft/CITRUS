387ad3275c6f9255b7c3e022b0ce85655e542a05
KentCShit <94623626+RaphBarniques@users.noreply.github.com>
2024-11-19 07:32:19 -0500

    Corrected score counting

--- CITRUS/urls.py
+++ CITRUS/urls.py
@@ -16,10 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='/Citrus', permanent=True)),
     path('admin/', admin.site.urls),
     path('Citrus/', include('CitrusApp.urls')),
     path('Citrus/', include('django.contrib.auth.urls')),
